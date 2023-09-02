903eca6e6186d375dc37750caf2283082ec40cd9
Viviane Araujo <developer.viviane@gmail.com>
2023-09-02 20:56:14 -0300

    Implemented DogItem component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import placeholderImg from "./images/dog_walking.svg";
 import Loading from "./components/Loading";
 import Empty from "./components/Empty";
+import DogItem from "./components/DogItem";
 
 function App() {
   const [breeds, setBreeds] = useState([]);
@@ -92,12 +93,18 @@ function App() {
           {dogImages.length > 0 &&
             !isLoading &&
             dogImages.map((imgSrc, index) => (
-              <img
-                key={`${index}-${selectedBreed}`}
-                src={imgSrc}
-                className="img-thumbnail w-25"
-                alt={`${selectedBreed} ${index + 1} of ${dogImages.length}`}
+              <DogItem
+                imgSrc={imgSrc}
+                index={index}
+                selectedBreed={selectedBreed}
+                dogImages={dogImages}
               />
+              //<img
+              //  key={`${index}-${selectedBreed}`}
+              //  src={imgSrc}
+              //  className="img-thumbnail w-25"
+              //  alt={`${selectedBreed} ${index + 1} of ${dogImages.length}`}
+              ///>
             ))}
         </div>
       </main>
